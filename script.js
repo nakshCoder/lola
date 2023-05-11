263a4068feeea7c10bbd7ad0bfdc3d3818277f77
nakshCoder <nakshusharm0@gmail.com>
2023-05-11 13:36:14 +0530

    added more filter items

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ $(".modal-dialog")
   .on("show.bs.modal", function (e) {
     $(e.relatedTarget.attributes["data-target"].value).appendTo("body");
   });
+
+var accItem = document.getElementsByClassName("accordionItem");
+var accHD = document.getElementsByClassName("accordionItemHeading");
+for (i = 0; i < accHD.length; i++) {
+  accHD[i].addEventListener("click", toggleItem, false);
+}
+function toggleItem() {
+  var itemClass = this.parentNode.className;
+  for (i = 0; i < accItem.length; i++) {
+    accItem[i].className = "accordionItem close";
+  }
+  if (itemClass == "accordionItem close") {
+    this.parentNode.className = "accordionItem open";
+  }
+}
+
+const value = document.querySelector("#value");
+const input = document.querySelector("#value_input");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+  value.textContent = event.target.value;
+});
